7ee3cbcc4b2abc46a4e6b1843a79d4ba39e38f6c
giedrius <giedriustum@gmail.com>
2022-08-16 22:47:47 +0300

    Phpunit installed

---
+++ tests/Unit/PlanetGrowTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Galaktika\Data\Planet;
+use PHPUnit\Framework\TestCase;
+
+class PlanetGrowTest extends TestCase
+{
+    public function testPlanetGrow()
+    {
+        $planet = new Planet();
+
+        $this->assertTrue(true);
+    }
+
+}
