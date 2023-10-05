93d9b2e9a5ebde2c3d914f2902c1d7709679dea0
giedrius <giedriustum@gmail.com>
2023-10-05 20:58:45 +0300

    Sequence generator.

---
+++ sources/Galaktika/V2/Math/SequenceGenerator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Galaktika\V2\Math;
+
+class SequenceGenerator
+{
+    public static function generate(array $randoms) : array {
+        $size = count($randoms);
+
+        $indexes = [];
+        for ($i=0; $i < $size; $i++) {
+           $indexes[$i] = $i;
+        }
+
+        $result=[];
+        foreach ($randoms as $random) {
+            $selected = intval($random * $size );
+            $result[] = $indexes[$selected];
+            $indexes[$selected] = $indexes[$size-1];
+            // for debugging
+            unset($indexes[$size-1]);
+            $size --;
+        }
+
+        return $result;
+    }
+}
---
+++ tests/Galaktika/V2/Math/SequenceGeneratorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Galaktika\V2\Math;
+
+use Galaktika\V2\Math\SequenceGenerator;
+use PHPUnit\Framework\TestCase;
+
+class SequenceGeneratorTest extends TestCase
+{
+
+    /**
+     * @dataProvider provide
+     */
+    public function testGenerator($size)
+    {
+        $randomValues = [];
+        for ($i = 0; $i < $size; $i++) {
+            $randomValues[] = lcg_value();
+        }
+
+        $sequence = SequenceGenerator::generate($randomValues);
+
+        $this->assertCount($size, $sequence);
+
+        if ($size <= 10) {
+            echo sprintf("Sequence: %s", join(',', $sequence));
+        }
+
+        $uSequence = array_unique($sequence);
+        $this->assertEquals($uSequence, $sequence);
+
+        $sequence2 = SequenceGenerator::generate($randomValues);
+
+        $this->assertEquals($sequence, $sequence2);
+    }
+
+    public function provide(): array
+    {
+        return [
+            'test10' => [
+                'size' => 10,
+            ],
+            'test1' => [
+                'size' => 1,
+            ],
+            'test3' => [
+                'size' => 3,
+            ],
+            'test100' => [
+                'size' => 100,
+            ],
+            'test0' => [
+                'size' => 0,
+            ],
+        ];
+    }
+}
