e4fb2944a9706abd0980f9cfba9b5570a059d2da
giedrius <giedrius@inkodus.lt>
2024-02-21 16:25:22 +0200

    Preparing battles test.

--- tests/Galaktika/V2/London/Turn/BattlesInGameTest.php
+++ tests/Galaktika/V2/London/Turn/BattlesInGameTest.php
@@ -2,12 +2,45 @@
 
 namespace Tests\Galaktika\V2\London\Turn;
 
+use Galaktika\SimpleIdGenerator;
+use Galaktika\V2\Data\DiplomacyMap;
+use Galaktika\V2\Data\GameSettings;
+use Galaktika\V2\Data\GameTurn;
+use Galaktika\V2\Game\TurnMaker;
 use PHPUnit\Framework\TestCase;
 
 class BattlesInGameTest extends TestCase
 {
-    public function testBattlesInTurn() {
+    /**
+     * @dataProvider provideTestingGames
+     */
+    public function testBattlesInTurn(GameTurn $game, GameTurn $expectedGame)
+    {
+        $idGenerator = new SimpleIdGenerator();
+        $gameSettings = new GameSettings();
+        $diplomacyMap = new DiplomacyMap();
+        $turnMaker = new TurnMaker($game, $idGenerator, $gameSettings);
+        $turnMaker->setDiplomacyMap($diplomacyMap);
+
+        $newGame = $turnMaker->makeTurn();
+
         // TODO
+
         $this->assertTrue(true);
+    }
+
+    public static function provideTestingGames(): array
+    {
+        return [
+            'test material' => [
+                'game' => (new GameTurn())
+                // TODO
+                ,
+                'expectedGame' => (new GameTurn())
+                // TODO
+                ,
+
+            ],
+        ];
     }
 }
