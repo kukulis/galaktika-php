ea79f4d6194c1e23200e5c1149eff397e6bd0a1e
giedrius <giedrius@inkodus.lt>
2024-03-10 23:18:16 +0200

    Failing battles test.

--- tests/Galaktika/V2/London/Turn/BattlesInGameTest.php
+++ tests/Galaktika/V2/London/Turn/BattlesInGameTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests\Galaktika\V2\London\Turn;
 
+use Galaktika\Data\Battle;
 use Galaktika\SimpleIdGenerator;
+use Galaktika\Util\SingletonsContainer;
+use Galaktika\V2\Battle\BattleReport;
 use Galaktika\V2\Data\DiplomacyMap;
+use Galaktika\V2\Data\Fleet;
 use Galaktika\V2\Data\GameSettings;
 use Galaktika\V2\Data\GameTurn;
+use Galaktika\V2\Data\Location;
 use Galaktika\V2\Game\TurnMaker;
 use PHPUnit\Framework\TestCase;
 
@@ -13,8 +18,10 @@ class BattlesInGameTest extends TestCase
 {
     /**
      * @dataProvider provideTestingGames
+     *
+     * @param BattleReport[] $expectedBattleReports
      */
-    public function testBattlesInTurn(GameTurn $game, GameTurn $expectedGame)
+    public function testBattlesInTurn(GameTurn $game, GameTurn $expectedGame, array $expectedBattleReports)
     {
         $idGenerator = new SimpleIdGenerator();
         $gameSettings = new GameSettings();
@@ -24,9 +31,12 @@ class BattlesInGameTest extends TestCase
 
         $newGame = $turnMaker->makeTurn();
 
-        // TODO
+        $expectedBornIds = array_map ( fn(Fleet $f)=> $f->getBornId(), $expectedGame->getFleets());
+        $bornIds = array_map ( fn(Fleet $f)=> $f->getBornId(), $newGame->getFleets());
 
-        $this->assertTrue(true);
+        $reports = $turnMaker->getBattleReports();
+        $this->assertEquals($expectedBattleReports, $reports);
+        $this->assertEquals($expectedBornIds, $bornIds);
     }
 
     public static function provideTestingGames(): array
@@ -34,12 +44,36 @@ class BattlesInGameTest extends TestCase
         return [
             'test material' => [
                 'game' => (new GameTurn())
-                // TODO
+                    ->setFleets([
+                        SingletonsContainer::instance()->getSingleton('fleet1', fn() => (new Fleet())
+                            ->setLocation(
+                                (new Location())
+                                    ->setX(10)
+                                    ->setY(10)
+                            )
+                            ->setBornId('origId1')
+                        ),
+                        SingletonsContainer::instance()->getSingleton('fleet2', fn() => (new Fleet())
+                            ->setLocation(
+                                (new Location())
+                                    ->setX(10)
+                                    ->setY(10)
+                            )
+                            ->setBornId('origId2')
+                        )
+                    ])
                 ,
                 'expectedGame' => (new GameTurn())
-                // TODO
+                    ->setFleets([
+                        SingletonsContainer::instance()->getSingleton('fleet1') // this will be a different fleet with the same base
+                    ])
                 ,
 
+                'expectedBattleReports' => [
+                    (new BattleReport())
+                        ->setFleetA(SingletonsContainer::instance()->getSingleton('fleet1'))
+                        ->setFleetB(SingletonsContainer::instance()->getSingleton('fleet2')),
+                ]
             ],
         ];
     }
